90978b0eb58d2d0cd5936ea70dc669fe587f46ec
Thor Torvund <thor.torvund@nav.no>
2022-05-04 12:35:29 +0200

    Oppgave 3 ...

--- src/oppgave_3.ps1
+++ src/oppgave_3.ps1
@@ -1,8 +1,13 @@
-Write-Host 'XXX'
 
 $shuffle = Invoke-RestMethod -Uri "http://nav-deckofcards.herokuapp.com/shuffle"
-$deck = $shuffle.Content | Out-String | ConvertFrom-Json
-foreach ($x in $deck)
+
+foreach ($kort in $shuffle)
 {
-    Write-Host "$x.suit $x.value"
+
+    $trukket = $($kort.suit[0])+$($kort.value)
+    Write-Host "$trukket"
+
+    $konkat_kort = $konkat_kort + $trukket + ","
 }
+
+Write-Host $konkat_kort.Substring(0,$konkat_kort.Length-1)
