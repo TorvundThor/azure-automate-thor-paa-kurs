c5b805cfd5276baa7654da408a2341492118603e
Thor Torvund <thor.torvund@nav.no>
2022-05-11 14:41:16 +0200

    Oppave 8

---
+++ src/oppgave_8.ps1
@@ -0,0 +1,140 @@
+[CmdletBinding()]
+param (
+    [Parameter(HelpMessage = "URL til kortstokk",Mandatory)]
+    [Uri]
+    $shuffleUri
+)
+
+$ErrorActionPreference = 'Stop'
+
+$shuffle = Invoke-Webrequest -Uri $shuffleUri
+
+$shuffleJson = $shuffle.Content
+
+$kortstokk = ConvertFrom-Json -InputObject $shuffleJson
+
+function skrivUt {
+    param (
+        [object[]]
+        $megListe,
+        [object[]]
+        $magnusListe,
+        [string]
+        $vant
+    ) 
+        $trukket = $megListe + $magnusListe
+        Write-Output "trukket: " $(listKort -stokk $trukket)
+        Write-Output "summen av kortene er:  $(summerKort -kortstokk $trukket)"
+
+        Write-Output "meg: " $(listKort -stokk $megListe)
+        Write-Output "Magnus: " $(listKort -stokk $magnusListe)
+
+        Write-Output "summen av kortene er:  $(summerKort -kortstokk $megListe)"
+        Write-Output "summen av kortene er:  $(summerKort -kortstokk $magnusListe)"
+
+        Write-Output "$vant"
+
+        Write-output "meg | $(summerKort -kortstokk $megListe) | $(listKort -stokk $megListe)"
+        Write-output "Magnus | $(summerKort -kortstokk $magnusListe) | $(listKort -stokk $magnusListe)"
+        
+}
+function listKort  {
+    [OutputType([string])]
+    param (
+        [object[]]
+        $stokk
+    )
+    foreach ($kort in $stokk)
+    {
+
+        $streng = $streng + $($kort.suit[0])+$($kort.value) + ","
+ 
+    }
+    return $streng.Substring(0,$streng.Length-1)
+}
+
+function summerKort {
+    param (
+        [object[]]
+        $kortstokk
+    )
+    $summen = 0
+    foreach ($x in $kortstokk) {
+        $summen += 
+        switch ($x.value) {
+            "J" { 10 } 
+            "Q" { 10 }
+            "K" { 10 }
+            "A" { 11 }
+            Default {$x.value}
+        }
+    }
+    return $summen
+}
+
+
+Write-Output "kortstokk: " $(listKort -stokk $kortstokk)
+
+Write-Output "summen av kortene er:  $(summerKort -kortstokk $kortstokk)"
+
+# trekker 2 kort for meg
+$i = 1
+$meg = $kortstokk[0..$i]
+$i = $i + 1
+$kortstokk = $kortstokk[2..$kortstokk.Count]
+
+# trekker 2 kort for magnus
+$i = 1
+$magnus = $kortstokk[0..$i]
+$i = $i + 1
+$kortstokk = $kortstokk[$i..$kortstokk.Count]
+
+if ($(summerkort -kortstokk $meg) -eq 21 -and $(summerkort -kortstokk $magnus) -eq 21)
+    {
+    skrivUt -megListe $meg -magnusListe $magnus -vant "Draw"
+    exit
+    }
+elseif ($(summerkort -kortstokk $meg) -eq 21) {
+    skrivUt -megListe $meg -magnusListe $magnus -vant "Vinner: Jeg"
+    exit
+}
+elseif ($(summerkort -kortstokk $meg) -eq 21) {
+    skrivUt -megListe $meg -magnusListe $magnus -vant "Vinner: Magnus"
+    exit
+}
+
+if ($(summerkort -kortstokk $meg) -lt 17)
+{
+while ($(summerkort -kortstokk $meg) -lt 21)
+    {
+        $meg = $meg + $kortstokk[0..1]
+        $kortstokk = $kortstokk[2..$kortstokk.Count]
+    }
+}
+
+if ($(summerkort -kortstokk $meg) -gt 21)
+{
+    skrivUt -megListe $meg -magnusListe $magnus -vant "Vinner: Magnus"
+    exit
+}
+
+if ($(summerkort -kortstokk $magnus) -lt 17 -and $(summerKort -kortstokk $meg) -le 21 )
+{
+while ($(summerkort -kortstokk $magnus) -lt 21)
+    {
+        $i = 0
+        $magnus = $magnus + $kortstokk[0..$i]
+        $i = $i + 1
+        $kortstokk = $kortstokk[$i..$kortstokk.Count]
+        if ($(summerkort -kortstokk $magnus) -ge 17) {break}
+    }
+}
+
+if ($(summerKort -kortstokk $meg) -gt $(summerKort -kortstokk $magnus)) 
+    {$vinner = "Vinner: meg"}
+elseif ($(summerKort -kortstokk $meg) -lt $(summerKort -kortstokk $magnus)) 
+    {$vinner = "Vinner: Magnus"}
+else 
+    {$vinner = "Draw"}
+
+skrivUt -megListe $meg -magnusListe $magnus -vant $vinner
